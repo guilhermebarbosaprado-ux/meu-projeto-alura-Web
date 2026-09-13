33334cc5d1a49c118964f6151fa6ebb9d0f755d3
guilhermebarbosaprado-ux <guilherme.barbosa.prado@escola.pr.gov.br>
2026-09-13 11:15:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ botões.forEach(function (botão) {
             texto.textContent++;
         }
     })
+
+    const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+
+    btnTemaEscuro.addEventListener("click", mudaTema);
+
+    function mudaTema() {
+        const corpoPagina = document.body;
+        if (corpoPagina.classList.contains("tema-escuro")) {
+            corpoPagina.classList.remove("tema-escuro");
+        } else {
+            corpoPagina.classList.add("tema-escuro");
+        }
+    }
